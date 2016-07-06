182d0bb5d3b66f84fb175be136d02f94b55c8da7
Steven Moss <stevencraigmoss@outlook.com>
2016-07-06 11:52:48 -0400

    Ryu is jQuery ready

--- js/app.js
+++ js/app.js
@@ -1,4 +1,39 @@
 $(document).ready(function(){
-    $("#keyboard").keydown(function(e){
-        if (e.keycode == 88){
+    $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function(){
+        $('.ryu-ready').hide();
+        $('.ryu-still').show();
+    })
+    .mousedown(function(){
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            400,
+            function(){
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
+    })
+
+    .mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
+
+    $('body').keydown(function(e){
+        if (e.which == 88) {
+            console.log('You pressed the x key');
+            }
+        });
 });
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+};
